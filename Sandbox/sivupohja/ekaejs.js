7bee4d8cacf3a08b345cde05edd0b904e10e1864
JariKovis <itse@jarikovalainen.com>
2023-03-23 08:23:56 +0000

    Sandbo Sivupohja päivitetty

---
+++ Sandbox/sivupohja/ekaejs.js
@@ -0,0 +1,50 @@
+var express = require("express");
+var app = express();
+const port = process.env.PORT || 3001;
+
+// otetaan EJS käyttöön
+app.set("view engine", "ejs");
+
+// Tällä pakotetaan sivupohja tuottamaan sisennettyä, kaunista HTML:ää. 
+// Tuotantokäytössä asetus voi olla false jolloin sivujen koko pienenee hieman
+app.locals.pretty = true;
+
+var etusivu = {
+    heading: "ETUSIVU",
+    content: "Sisältö"
+}
+app.get("/", function (req, res) {
+    res.render("pages/index", etusivu);
+});
+// Määritellään muuttuja
+var tervehdys = { teksti: "Sivupohjien käyttö on helppoa!!!" };
+
+app.get("/tervehdys", function (req, res) {
+    // välitetään muuttuja sivupohjalle nimeltä "terve.ejs"
+    res.render("pages/terve", tervehdys);
+});
+
+var ostokset = {
+    otsikko: "Ostoslista",
+    taulu: ["banaania", "omenaa", "päärynää", "kesäkurpitsaa"]
+};
+
+app.get("/ostokset", function (req, res) {
+    res.render("pages/ostokset", ostokset);
+});
+
+// Passing an array as data
+var moredata = [
+    { name: "John", age: 25 },
+    { name: "Mike", age: 42 },
+    { name: "Samantha", age: 51 }
+];
+app.get("/users", function (req, res) {
+    res.render("pages/users", { users: moredata });
+});
+
+// Web-palvelimen luonti Expressin avulla
+app.listen(port, function () {
+    console.log("Example app listening on port: " + port);
+});
+
